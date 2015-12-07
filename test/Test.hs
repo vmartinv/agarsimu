0d3aa3ceb19bc8b2f1a49329eba93bd9296f4d2c
Martin Villagra <mvillagra0@hotmail.com>
2015-12-07 18:34:17 -0300

    improved cabal file

--- test/Test.hs
+++ test/Test.hs
@@ -1,5 +1,4 @@
 module Main where
-
 import AgarSimu
 import Prelude hiding ((.), id)
 import Control.Wire
@@ -11,8 +10,6 @@ testScene :: Scene
 testScene = [(derecha, Player (show i) [Bola (20, (20+(fromInteger i)*30)) 200] (rgbColor 255 ((fromIntegral i)*40) 40)) | i <- [0..2]]
          ++ [(super, Player (show i) [Bola (120, (20+(fromInteger (i-2))*50)) 300] (rgbColor 40 ((fromIntegral i)*40) 255)) | i <- [2..3]]
 
-
-
 derecha :: AI
 derecha = pure (1, 0)
 
@@ -23,10 +20,7 @@ vibrar = for 0.2 . pure (0, 1) --> for 0.2 . pure (0, -1) --> vibrar
 derechayesperar :: AI
 derechayesperar =  for 0.5 . derecha  --> for 0.5 . pure (0, 0)
                     --> derechayesperar
-                    
-                    
-                                        
-                    
+   
 super :: AI
 super = (for 1 . pure (1, 1)) --> pure (0.01, 0.01)
 
